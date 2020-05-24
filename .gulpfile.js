024b9036cbb59ff94e4aa47f93b83697f6d9407f
Alexander Sabanov <alexsab@mail.ru>
2020-05-24 22:54:53 +0400

    upd npm package
    add style for .rand block on error popup
    upd gulp (ren lazyload)

--- .gulpfile.js
+++ .gulpfile.js
@@ -4,7 +4,7 @@
 				base.newstreetpunk_service_avtobum + '/libs/Magnific-Popup-master/jquery.magnific-popup.js',
 				base.newstreetpunk_service_avtobum + '/libs/animate/animate-css.js',
 				base.newstreetpunk_service_avtobum + '/libs/jquery.mb.YTPlayer.min.js',
-				base.newstreetpunk_service_avtobum + '/libs/lazyload.min.js',
+				base.newstreetpunk_service_avtobum + '/libs/lazyload.js',
 				base.newstreetpunk_service_avtobum + '/libs/waypoint.js',
 				base.newstreetpunk_service_avtobum + '/js/map.js',
 				base.newstreetpunk_service_avtobum + '/js/service-common.js', // Custom scripts. Always at the end
